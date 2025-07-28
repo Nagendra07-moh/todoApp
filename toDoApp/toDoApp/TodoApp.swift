a053817299958cd0084cdae9b89c5adc3ff37b92
Nagendra <nagendra.mohan.5@gmail.com>
2025-07-29 03:49:42 +0530

    initiated++

--- toDoApp/toDoApp/TodoApp.swift
+++ toDoApp/toDoApp/TodoApp.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct TodoApp: View {
+    @State var item: String = ""
+    
     var body: some View {
-        VStack {
+        VStack (alignment: .center, spacing: 16) {
             Text("TO DO!")
+                .font(.largeTitle)
+
+            HStack{
+                TextField("Add a new task", text:$item)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+                .border(Color.gray, width: 2)
+                
+                .padding(.horizontal , 8)
+                
+                Button("Add"){
+                   addNameFunction()
+                }
+                .padding(.horizontal , 18)
+                    
+            
+            }
+            
+            Spacer()
+                
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .padding()
+    }
+    
+    
+    func addNameFunction(){
+        print(item)
     }
 }
 
